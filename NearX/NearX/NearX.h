c7cacf4d20388cecc9c65c2d84a8e59781dae63d
Kushagra Sinha <kushagra@getwalk.in>
2020-02-05 10:10:30 +0530

    Framework initial setup

---
+++ NearX/NearX/NearX.h
@@ -0,0 +1,19 @@
+//
+//  NearX.h
+//  NearX
+//
+//  Created by Kushagra on 05/02/20.
+//  Copyright © 2020 First Walkin Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NearX.
+FOUNDATION_EXPORT double NearXVersionNumber;
+
+//! Project version string for NearX.
+FOUNDATION_EXPORT const unsigned char NearXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NearX/PublicHeader.h>
+
+
---
+++ NearX/NearX/Service.swift
@@ -0,0 +1,20 @@
+//
+//  Service.swift
+//  NearX
+//
+//  Created by Kushagra on 05/02/20.
+//  Copyright © 2020 First Walkin Technologies. All rights reserved.
+//
+
+import Foundation
+
+public class Service {
+    
+    private init() {}
+    
+    public static func sayHello() -> String {
+    
+        return "Hello there!"
+        
+    }
+}
