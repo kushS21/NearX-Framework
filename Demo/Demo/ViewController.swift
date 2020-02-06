06c1258a69c10d8730577f1dd337a87ae1fbb0ab
Kushagra Sinha <kushagra@getwalk.in>
2020-02-06 12:02:59 +0530

    basic function

--- Demo/Demo/ViewController.swift
+++ Demo/Demo/ViewController.swift
@@ -10,10 +10,32 @@ import UIKit
 import NearX
 
 class ViewController: UIViewController {
+    
+    let geofence: Geofence = Geofence()
+    
+    // Provide your auth key
+    let AUTH_KEY = "";
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // Calls a simple function to initialize and track user.
+        
+        startTracking()
+    }
+    
+    func startTracking()
+    {
+        let token = UserDefaults.standard.string(forKey: "FCM_TOKEN")!
+        
+        GeofenceUtils.setName(name: "Walkin User")
+        GeofenceUtils.setMobileNumber(mobileNumber: "9090909090")
+        GeofenceUtils.setFCMToken(fcmToken: token)
+        GeofenceUtils.setAuthKey(authKey: AUTH_KEY)
+        
+        geofence.initializeGeofences()
     }
 
 
